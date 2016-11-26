478dd1db8deeb92c7bd2a2d5e4bdd6d4fc301110
tom c <tom c>
2016-11-26 16:15:13 -0500

    wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,57 @@ var currentWord = ["x"];
 var guessesLeft = 10;
 var alreadyGuessed = [""];
 
+
 // FUNCTIONS (These are bits of code that we will call upon to run when needed)
 // ==================================================================================================
 
+	function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+
+    return true;
+	}
 
 // MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
 // ==================================================================================================
 
 var random = Math.floor(Math.random() * wordChoices.length);
 currentWord = wordChoices[random];
-currentWord.split("");
+var answerArray = currentWord.split("");
+console.log(currentWord);
 
-for(var i = 0; i < currentWord.length; i++){
-	console.log(currentWord[i])
-}
+ var hiddenArray = [""];
+ for(var i = 0; i < answerArray.length; i++){
+	hiddenArray[i] = "_";	
+	}	
+ console.log(hiddenArray);
 
 
 document.onkeyup = function(event){
 
-var letter = String.fromCharCode(event.KeyCode).toLowerCase();
+var letter = String.fromCharCode(event.keyCode).toLowerCase();
+	//console.log(alert("you entered " + letter));
+
+for(var j = 0; j < answerArray.length; j++){
+	console.log("answerArray in j for loop " + answerArray);
+	
+	if (letter === answerArray[j]){
+		hiddenArray[j] = answerArray[j];
+		console.log("new hidden array " + hiddenArray)
+	}
+}
+
+	if(arraysEqual(hiddenArray, answerArray)){
+		alert("hurray"); 
+		wins++;
+	}
+
+	console.log("wins = " + wins);
+	console.log("hiddenArray " + hiddenArray);
+	console.log("answerArray " + answerArray);
+
 }
